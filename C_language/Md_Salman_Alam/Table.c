e0a70d4d6bd5a1658b354923ba2110dc78983ac8
Salman-codehub <mdsalman67953@gmail.com>
2024-09-25 11:57:18 +0530

    Update Table.c
    Using this code we can find easily table of any numbers

--- C_language/Md_Salman_Alam/Table.c
+++ C_language/Md_Salman_Alam/Table.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main()
+{int n,i,num1;
 
+printf("\n\t\tTable");
+
+printf("\n\nenter any number:");
+scanf("%d",&num1);
+
+
+for(i=1;i<=10;i++)
+printf("\n\t\t%d*%d=%d",num1,i,num1*i);
+printf("\n\n Thank you ");
+
+return 0;
+}
+    
